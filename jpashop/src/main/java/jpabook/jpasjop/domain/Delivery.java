64a36d5276cab5a104393141dbcdb86498e7303e
Leeww03 <hjww0522@gmail.com>
2021-03-15 12:20:07 +0900

    실전 예제 3 - 다양한 연간관계 매핑

---
+++ jpashop/src/main/java/jpabook/jpasjop/domain/Delivery.java
@@ -0,0 +1,22 @@
+package jpabook.jpasjop.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Delivery {
+    @Id @GeneratedValue
+    private long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    private String city;
+
+    private String street;
+
+    private String zipcode;
+
+    @Enumerated
+    private DeliveryStatus status;//배송상태
+
+}
